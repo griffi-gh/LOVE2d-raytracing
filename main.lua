61e7b4cebb5782954c12f5e27f88edccd8ed227a
griffi-gh <45996170+griffi-gh@users.noreply.github.com>
2020-03-23 22:54:26 +0200

    update

--- main.lua
+++ main.lua
@@ -1,15 +1,29 @@
+-------------------------------------------------------------------------------------
+boxes={                                                                            --
+  {x=100,y=100,w=30,h=30},  --top left box                                         --
+  {x=700,y=300,w=50,h=80},  --top right box                                        --
+  {x=600,y=80,w=100,h=40},  --bottom right box                                     --
+  {x=300,y=100,w=210,h=10}, --room-top wall                                        --
+  {x=500,y=150,w=10,h=60},  --room-right wall                                      --
+  {x=300,y=200,w=200,h=10}, --room-bottom wall                                     --
+  {x=300,y=100,w=10,h=110}, --room-left wall                                       --
+}                                                                                  --
+raySpeed=4                  --bigger value=better fps and speed, worse accuracy    --
+timesteps=100               --bigger value=worse fps, better speed                 --
+raysCount=360*2             --bigger value=worse fps,more rays/accuracy            --
+-------------------------------------------------------------------------------------
+
 rays={}
 rayStart={x=399,y=299}
-boxes={{x=100,y=100,w=30,h=30},{x=700,y=300,w=50,h=80},{x=600,y=80,w=100,h=40}}
-
-raySpeed=3 --bigger value=better fps and speed, worse accuracy
-timesteps=250 --bigger value=worse fps, better speed
-raysCount=360*4 --bigger value=worse fps,more rays/accuracy
 
 function bts(b,st,sf) st=st or 'true' sf=sf or 'false' if(b)then return st else return sf end end 
 
+
+--a2=0
+
 function spawn(filter)
   local filter=filter or function(a,i,px,py) return true end
+  --local filter=filter or function(a,i,px,py) return a>a2 and a<a2+90 end --90* FOV FILTER (1/4)
   rays={}
   local imax=raysCount
   for i=1,imax do
@@ -25,7 +39,7 @@ function spawn(filter)
     end
     
     if filter(i/imax*360,i,px,py) then
-      rays[#rays+1]={x=rayStart.x,y=rayStart.y,a={x=math.cos(math.rad(px*360)),y=math.sin(math.rad(py*360))}}
+      rays[#rays+1]={x=rayStart.x,y=rayStart.y,a={x=math.cos(math.rad(px*360)),y=math.sin(math.rad(py*360)),l=0}}
     end
   end
 end
@@ -38,7 +52,43 @@ function love.load()
 end
 
 function love.update(dt)
-  
+    local w,h=love.graphics.getWidth(),love.graphics.getHeight()
+    
+    moving=false
+    if(m1)then
+      if not(rayStart.x==mx and rayStart.y==my) and finished then
+        rayStart={x=mx,y=my}
+        spawn()
+        moving=true
+      end
+    end
+    
+    ct=timesteps
+    if finished and not(moving) then
+      ct=1
+    end
+    for it=1,ct do
+      love.graphics.setColor(0.5,0.5,0.5)
+      traced=0
+      for i,v in ipairs(rays) do
+        local blocked=false
+        for i2,v2 in ipairs(boxes) do
+          if (v.x>v2.x and v.x<v2.x+v2.w and v.y>v2.y and v.y<v2.y+v2.h)or(v.x<0 or v.x>=w or v.y<0 or v.y>=h) then
+            if not(blocked)then
+              traced=traced+1
+              blocked=true
+            end
+          end
+        end
+        if not(blocked) then
+          v.x=v.x+v.a.x*raySpeed
+          v.y=v.y+v.a.y*raySpeed
+        end
+        if(it==ct)then
+          finished=(#rays==traced)
+        end
+      end
+    end
 end
 
 function love.mousemoved(x,y,dx,dy)
@@ -55,48 +105,26 @@ function love.keypressed(key)
       love.window.setVSync(1)
     end
   end
+  if key=='c' then
+    boxes[#boxes+1]={x=love.math.random(w),y=love.math.random(h),w=love.math.random(20,w/8),h=love.math.random(20,h/8)}
+    spawn()
+  end
+  --[[if key=='o' then
+    a2=a2+15
+    spawn()
+  elseif key=='p' then
+    a2=a2-15
+    spawn()
+  end]]
 end
 
 function love.draw()
   m1=love.mouse.isDown(1)
   w,h=love.graphics.getWidth(),love.graphics.getHeight()
   
-  moving=false
-  if(m1)then
-    if not(rayStart.x==mx and rayStart.y==my) then
-      rayStart={x=mx,y=my}
-      spawn()
-      moving=true
-    end
+  for i,v in ipairs(rays) do
+    love.graphics.line(rayStart.x,rayStart.y,v.x,v.y)
   end
-  
-  local ct=timesteps
-  if finished and not(moving) then
-    ct=1
-  end
-  for it=1,ct do
-    love.graphics.setColor(0.5,0.5,0.5)
-    traced=0
-    for i,v in ipairs(rays) do
-      local blocked=false
-      for i2,v2 in ipairs(boxes) do
-        if (v.x>v2.x and v.x<v2.x+v2.w and v.y>v2.y and v.y<v2.y+v2.h)or(v.x<0 or v.x>=w or v.y<0 or v.y>=h) then
-          if not(blocked)then
-            traced=traced+1
-            blocked=true
-          end
-        end
-      end
-      if not(blocked) then
-        v.x=v.x+v.a.x*raySpeed
-        v.y=v.y+v.a.y*raySpeed
-      end
-      if(it==ct)then
-        love.graphics.line(rayStart.x,rayStart.y,v.x,v.y)
-      end
-    end
-  end
-  finished=(#rays==traced)
   
   love.graphics.setColor(0.75,0,0)
   for i,v in ipairs(boxes) do
@@ -110,9 +138,9 @@ function love.draw()
   
   love.graphics.print(
     love.timer.getFPS()..' FPS\tvsync'..
-    bts(vsyncOff,' off',' on')..' (V)\ttraced:'..traced..
+    bts(vsyncOff,' off',' on')..'\ttraced:'..traced..
     '/'..#rays..'/'..raysCount..'\t'..bts(finished,'OK!','BUSY')..
-    '\nraySpeed:'..raySpeed..'\ttimestep:'..ct
+    '\nraySpeed:'..raySpeed..'\ttimestep:'..ct..'\nC-CREATE RANDOM BOX\t V-VSYNC'
     ,0,0
   )
 end
